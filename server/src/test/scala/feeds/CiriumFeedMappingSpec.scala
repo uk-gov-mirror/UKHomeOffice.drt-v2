a191e55671cee615bf35585e56d25326be93b2bb
Ben Eppel <ben@beneppelsoftware.com>
2019-09-24 17:27:11 +0100

    DRT-5848 remove temp test

--- server/src/test/scala/feeds/CiriumFeedMappingSpec.scala
+++ server/src/test/scala/feeds/CiriumFeedMappingSpec.scala
@@ -25,27 +25,27 @@ class CiriumFeedMappingSpec extends TestKit(ActorSystem("testActorSystem", Confi
   "When successfully polling for CiriumArrivals I should get a stream of ArrivalFeedSuccess" >> {
     implicit val mat: ActorMaterializer = ActorMaterializer()
 
-    val ciriumFeed = new CiriumFeed("http://localhost:8080/statuses/stn")
-    val probe = TestProbe()
-
-    val cancellable = ciriumFeed.tickingSource.collect {
-      case afs: ArrivalsFeedSuccess =>
-        afs.arrivals.flights.map(a => {
-          println(s"flight: ${a.IATA}")
-          println(s"Scheduled: ${SDate(a.Scheduled).toISOString()}")
-          println(s"Act: ${a.Actual.map( d => SDate(d).toISOString())}")
-          println(s"Est: ${a.Estimated.map( d => SDate(d).toISOString())}")
-          println(s"EstChox: ${a.EstimatedChox.map( d => SDate(d).toISOString())}")
-          println(s"ActChox: ${a.ActualChox.map( d => SDate(d).toISOString())}")
-        })
-        afs
-    }.to(Sink.actorRef(probe.ref, "completed")).run()
-
-    probe.fishForMessage(30 seconds) {
-      case s: ArrivalsFeedSuccess if s.arrivals.flights.head.Scheduled == SDate("2019-07-15T11:05:00.000Z").millisSinceEpoch =>
-        println(s"Successfully got a result")
-        true
-    }
+//    val ciriumFeed = new CiriumFeed("http://localhost:8080/statuses/stn")
+//    val probe = TestProbe()
+//
+//    val cancellable = ciriumFeed.tickingSource.collect {
+//      case afs: ArrivalsFeedSuccess =>
+//        afs.arrivals.flights.map(a => {
+//          println(s"flight: ${a.IATA}")
+//          println(s"Scheduled: ${SDate(a.Scheduled).toISOString()}")
+//          println(s"Act: ${a.Actual.map( d => SDate(d).toISOString())}")
+//          println(s"Est: ${a.Estimated.map( d => SDate(d).toISOString())}")
+//          println(s"EstChox: ${a.EstimatedChox.map( d => SDate(d).toISOString())}")
+//          println(s"ActChox: ${a.ActualChox.map( d => SDate(d).toISOString())}")
+//        })
+//        afs
+//    }.to(Sink.actorRef(probe.ref, "completed")).run()
+//
+//    probe.fishForMessage(30 seconds) {
+//      case s: ArrivalsFeedSuccess if s.arrivals.flights.head.Scheduled == SDate("2019-07-15T11:05:00.000Z").millisSinceEpoch =>
+//        println(s"Successfully got a result")
+//        true
+//    }
 
     success
   }
